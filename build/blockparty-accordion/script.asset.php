9c71565c1ca11d3377083fc5173abb627618bd6f
Alexandre Sadowski <asadowski@beapi.fr>
2024-04-02 21:55:17 +0000

    Release version 1.0.1

---
+++ build/blockparty-accordion/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fcecd7094a04150900f5');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'beapi/blockparty-accordion',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '222597eb16271ec094a66a69f92c42f33c774be6',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'beapi/blockparty-accordion' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '222597eb16271ec094a66a69f92c42f33c774be6',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
