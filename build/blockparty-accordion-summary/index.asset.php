4d28473d58e097cc29b9cf61f071f9ce52c5c14f
Milan Ricoul <mricoul@beapi.fr>
2025-11-07 16:24:45 +0000

    Release version 1.0.5

---
+++ build/blockparty-accordion-summary/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'e261cabff34e3600a0b6');
---
+++ build/blockparty-accordion/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5d4b0596e14710c90174');
