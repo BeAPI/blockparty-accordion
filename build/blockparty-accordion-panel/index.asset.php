58095cbfdc9da8fba7f1d5876f4bd95e0d5d9418
Cédric Andrietti <cedric07@users.noreply.github.com>
2026-02-09 14:14:59 +0000

    Release version 1.0.9

---
+++ build/blockparty-accordion-panel/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-primitives'), 'version' => '8b3970d0bd269aef31e0');
---
+++ build/blockparty-accordion-summary/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '27869bcfa7bce51bf138');
