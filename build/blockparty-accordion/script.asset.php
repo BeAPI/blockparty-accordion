a984103dc37f69af9701cb07ff812ac9e8b0bc90
Cédric Andrietti <cedric07@users.noreply.github.com>
2026-02-09 10:50:47 +0000

    Release version 1.0.8

---
+++ build/blockparty-accordion/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7c1f57b9ade0ed163059');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'beapi/blockparty-accordion',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9f428ac0e399c53677a4639b627b74a6338b82f3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'beapi/blockparty-accordion' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9f428ac0e399c53677a4639b627b74a6338b82f3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
