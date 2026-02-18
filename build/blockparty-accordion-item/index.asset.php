d17ccb2fdb803b1181a9fe52ee8623326d3e1ea8
Milan Ricoul <mricoul@beapi.fr>
2026-02-18 15:52:59 +0000

    Release version 1.1.0

---
+++ build/blockparty-accordion-item/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'f4828c6647b8f37418db');
---
+++ build/blockparty-accordion-panel/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-primitives'), 'version' => '5ac74d6dc92fe274ee7d');
