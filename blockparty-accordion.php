eb5910ab9166fd6afc74a634238f985e4e76631e
mricoul <mricoul@beapi.fr>
2026-04-21 21:55:59 +0200

    chore: bump plugin version

--- blockparty-accordion.php
+++ blockparty-accordion.php
@@ -4,7 +4,7 @@
  * Description:       Accessible Accordion block for WordPress editor.
  * Requires at least: 6.2
  * Requires PHP:      8.1
- * Version:           1.1.0
+ * Version:           1.2.0
  * Author:            Be API Technical team
  * Author URI:        https://beapi.fr
  * License:           GPL-2.0-or-later
@@ -15,7 +15,7 @@
 
 namespace Blockparty\Accordion;
 
-define( 'BLOCKPARTY_ACCORDION_VERSION', '1.1.0' );
+define( 'BLOCKPARTY_ACCORDION_VERSION', '1.2.0' );
 define( 'BLOCKPARTY_ACCORDION_URL', plugin_dir_url( __FILE__ ) );
 define( 'BLOCKPARTY_ACCORDION_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BLOCKPARTY_ACCORDION_PLUGIN_DIRNAME', plugin_basename( __FILE__ ) );
